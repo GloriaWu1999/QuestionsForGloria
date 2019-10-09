d6e7c338ab579844ce2d8d50e2e11e9e13219526
Gloria <1459706627@qq.com>
2019-10-09 16:57:30 +0800

    Finish Task

---
+++ GreatestCommonDivider.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int getGCD(int m, int n)
+{
+	while (m!=n){
+		if(m>n){
+			m=m-n;
+		}
+		else {
+			n=n-m;
+		}
+	}
+	return m;
+}
+
+int main()
+{
+	int m = 0, n = 0;
+	scanf("%d %d", &m, &n);
+	int ans = getGCD(m, n);
+	printf("%d\n", ans);
+	if (m%ans == 0 && n%ans == 0) {
+		bool flag = 1;
+		for (int i = ans + 1; i < n; ++i) {
+			if (m%i == 0 && n%i == 0) {
+				flag = 0;
+			}
+		}
+		if (flag) {
+			printf("Correct!\n");
+		}
+		else {
+			printf("Wrong!\n");
+		}
+	}
+	return 0;
+}
